f780fe5ea212e6bc9dd7f96c42de83d4e180ce9b
kobbin003 <dkfeto@gmail.com>
2023-07-06 12:46:51 +0530

    server initialized

---
+++ trpc/server/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { publicProcedure, router } from "./trpc";
+import * as trpcExpress from "@trpc/server/adapters/express";
+const appRouter = router({
+	helloworld: publicProcedure.query(() => "Hello World!"),
+});
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
+
+const app = express();
+
+app.use("/", trpcExpress.createExpressMiddleware({ router: appRouter }));
+
+app.listen(3000, () => console.log("server is running on port 3000"));
