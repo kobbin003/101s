e555f88e2ebec4f07596bde8f20a11417f70b1b8
kobbin003 <dkfeto@gmail.com>
2023-10-23 20:12:18 +0530

    room and messages

--- socketio/server/src/index.ts
+++ socketio/server/src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 
 /** express app is attached to the httpserver */
 const httpServer = createServer(app);
-/** socketio server  is attached to the httpserver */
 app.get("/", (_, res) => {
 	res.send("hello socket");
 });
@@ -13,14 +12,42 @@ app.get("/", (_, res) => {
 /**  Express itself is capable of handling HTTP requests and can function
  * as a web server, Socket.IO, which is used for WebSocket-based communication,
  * requires a raw HTTP server instance to work efficiently. */
+/** socketio server  is attached to the httpserver */
 const io = new Server(httpServer, {
 	cors: {
-		origin: process.env.NODE_ENV == "production" ? "" : "http://localhost:5173",
+		origin: process.env.NODE_ENV == "production" ? "" : "*",
 	},
 });
 
 io.on("connection", (socket) => {
 	console.log("socket connection established", socket.id);
+
+	// listening for event "myevent"
+	socket.on("from-client-event", (arg, cb) => {
+		console.log("arg", arg);
+		cb("got it");
+
+		// emiting event "hi"
+		io.emit("from-server-event", `take back- ${arg}`);
+	});
+
+	/** join room */
+	socket.on("join-room", (arg, cb) => {
+		socket.join(arg);
+		cb(arg);
+		console.log(socket.rooms);
+		// io.to(arg).emit(`new-message`, "to all users");
+	});
+
+	/** listen for send-message event */
+	socket.on("send-message", ({ roomName, message }, cb) => {
+		console.log(roomName, message);
+		io.to(roomName).emit("new-message", message);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
 
 /** in this server we have:
